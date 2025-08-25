c1441b5989cd05d172278cbb3f6a4917c99f9191
efuentealbaCh <113476013+efuentealbaCh@users.noreply.github.com>
2025-08-25 15:46:42 -0400

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -24,7 +24,7 @@ class OrderController extends Controller
         }
 
         return view('orders.index', [
-            'orders' => $orders['orders'],
+            'orders' => $orders['orders'] != null ? $orders['orders'] : [],
         ]);
     }
 
